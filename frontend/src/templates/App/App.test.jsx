ef4dd2cbe23148cc31a942db0f31a2ed0b35fcef
Felipe Fardo <felipe.fardo@hotmail.com.br>
2023-02-09 18:16:25 -0300

    config jest, jest-styled-components and storybook

---
+++ frontend/src/templates/App/App.test.jsx
@@ -0,0 +1,18 @@
+import { screen } from '@testing-library/react';
+import Home from '.';
+import { renderTheme } from '../../styles/render-theme';
+import { theme } from '../../styles/theme';
+
+test('renders learn react link', () => {
+  // const {debug} = renderTheme(<Home />);
+  renderTheme(<Home />);
+  const headingContainer = screen.getByRole('heading', {
+    name: 'Hello',
+  }).parentElement;
+  expect(headingContainer).toHaveStyle({
+    background: theme.colors.secondaryBg,
+  });
+  expect(headingContainer).toHaveStyleRule('background', 'blue');
+
+  expect(headingContainer).toMatchSnapshot();
+});
