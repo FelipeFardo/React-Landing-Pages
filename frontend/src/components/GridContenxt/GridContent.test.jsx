71ff99d9f2e846a3e6ce90c714079db45287f5b1
Felipe Fardo <felipe.fardo@hotmail.com.br>
2023-02-17 17:44:58 -0300

    created Grid Content

---
+++ frontend/src/components/GridContenxt/GridContent.test.jsx
@@ -0,0 +1,19 @@
+import { screen } from '@testing-library/react';
+import { renderTheme } from '../../styles/render-theme';
+import { theme } from '../../styles/theme';
+import { GridContent } from '.';
+import mock from './mock';
+
+describe('<GridContenxt/>', () => {
+  it('should render', () => {
+    const { container } = renderTheme(<GridContent {...mock} />);
+    expect(container).toMatchSnapshot();
+  });
+
+  it('should render', () => {
+    const { container } = renderTheme(
+      <GridContent {...mock} background={undefined} />,
+    );
+    expect(container).toMatchSnapshot();
+  });
+});
